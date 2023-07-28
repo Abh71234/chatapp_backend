63eb4cba0f6a047e733be41e5d00a244a992a9fc
Abh71234 <96685214+Abh71234@users.noreply.github.com>
2023-07-28 19:00:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ const server=app.listen(PORT,
 const io = require("socket.io")(server, {
     pingTimeout: 60000,
     cors: {
-       origin: "https://chitchathub.onrender.com",
+       origin: '*',
       // credentials: true,
     },
   });
